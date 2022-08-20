b6e9d63671b1837e6c7ad58f3488f541f99816a0
Chris Condreay <condreaychris@gmail.com>
2022-08-20 16:21:33 -0700

    Add files via upload

--- server.js
+++ server.js
@@ -1,49 +1,15 @@
 const express = require('express');
 const path = require('path');
-const db = require('./db/db.json');
-const fs = require('fs');
-const {get} = require('http');
-
-const PORT = 8000;
 const app = express();
+
+const PORT = process.env.PORT || 8000;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(express.static(path.join(__dirname, 'public')));
 
-app.use(express.static('public'))
-
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'index.html'))
-})
-
-app.get('/notes', (req, res) => {
-  res.sendFile(path.join(__dirname, 'notes.html'))
-})
-
-app.get('/api/notes', (req, res) => {
-  res.json(db)
-});
-
-app.post('/api/notes', (req, res) => {
-  console.log(req.body);
-  const newNote = {
-    noteTitle: req.body.title,
-    noteText: req.body.text
-  }
-  console.log(newNote)
-  db.push(newNote)
-  console.log(db)
-  fs.writeFile('./db/db.json', JSON.stringify(db), (err) =>{
-        if (err) {
-            return console.log(err)
-        }; 
-        res.json(db)
-    })
-})
-
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, './index.html'));
-});
+require('./routes/apiRoutes.js')(app);
+require('./routes/htmlRoutes')(app);
 
 app.listen(PORT, () => {
   console.log(`App listening at http://localhost:${PORT}`);
